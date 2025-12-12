d2069b07120156f674f775555b6d27b2fd6aecbf
gailja-bit <ggwintra@gmail.com>
2025-12-12 12:18:27 +0700

    Code
    Putsaraphorn

---
+++ Putsaraphorn 1063/Mainmenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainMenu : MonoBehaviour
+{
+    public string gameSceneName = "02_Intro_Story";
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
